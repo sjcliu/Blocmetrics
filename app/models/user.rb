a785c554a200eb563acb26caeb5b365f4bd56cb5
Stanly Liu <sjcliu@gmail.com>
2016-04-04 22:54:42 +1000

    configuring devise

---
+++ app/models/user.rb
@@ -0,0 +1,32 @@
+class User < ActiveRecord::Base
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
+
+# Virtual attribute for authenticating by either username or email
+# This is in addition to a real persisted field like 'username'
+  attr_accessor :login
+  validates :username, :presence => true, :uniqueness => {:case_sensitive => false}
+  validate :validate_username
+
+  def self.find_first_by_auth_conditions(warden_conditions)
+    conditions = warden_conditions.dup
+    if login = conditions.delete(:login)
+      where(conditions).where(["lower(username) = :value OR lower(email) = :value", { :value => login.downcase }]).first
+    else
+      if conditions[:username].nil?
+        where(conditions).first
+      else
+        where(username: conditions[:username]).first
+      end
+    end
+  end
+
+
+  def validate_username
+    if User.where(email: username).exists?
+      errors.add(:username, :invalid)
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :user
   root 'welcome#index'
 
   get 'welcome/about'
