3bed703a74b9e22ad6d5f375fbd5ce6f26a0e98f
Stanly Liu <sjcliu@gmail.com>
2016-04-28 22:57:50 +0800

    seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,27 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+10.times do
+  User.create!(
+  username: Faker::Internet.user_name,
+  email: Faker::Internet.email,
+  password: Faker::Internet.password
+  )
+end
+
+20.times do
+  RegisteredApplication.create(
+    user_id: User.all.sample.id,
+    title: Faker::Lorem.word,
+    url: Faker::Internet.url
+  )
+end
+
+100.times do
+  Event.create(
+  name: Faker::Superhero.power,
+  registered_application_id: RegisteredApplication.all.sample.id
+  )
+end
